b464d367560afef1c97188de3d61d5a5af3c179d
Matheus <matheusegenio212@gmail.com>
2022-09-08 17:44:08 -0300

    feat: create Components

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,23 @@
 import Head from 'next/head'
 
+import { Container, Card, Avatar, Title, Subtitle } from '../styles'
+import { GlobalStyle } from '../styles/global'
+
 export default function Home() {
   return (
-    <div>
-      <Head>
-        <title>Contact Card</title>
-      </Head>
-    </div>
+    <>
+      <GlobalStyle />
+      <Container>
+        <Head>
+          <title>Contact Card</title>
+        </Head>
+
+        <Card>
+          <Title>Matheus Moura</Title>
+
+          <Subtitle>Dev Front-End</Subtitle>
+        </Card>
+      </Container>
+    </>
   )
 }
---
+++ styles/index.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  height: 100vh;
+
+  background: linear-gradient(
+      90deg,
+      #091e3a -31.08%,
+      #2f80ed 94.97%,
+      #2d9ee0 148.65%
+    ),
+    #ffffff;
+`
+
+export const Card = styled.div``
+
+export const Avatar = styled.img``
+
+export const Title = styled.h1``
+
+export const Subtitle = styled.h2``
