b0ce61fcefb6c9bc5eb0f3c8a6bb4571bc671a6f
Matheus <matheusegenio212@gmail.com>
2022-09-12 17:22:06 -0300

    feat: add image

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,19 @@
-import Head from 'next/head'
-
 import { Container, Card, Avatar, Title, Subtitle } from '../styles'
+
 import { GlobalStyle } from '../styles/global'
+
+import Image from 'next/image'
+
+import AvatarImage from '../assets/avatar.jpg'
 
 export default function Home() {
   return (
     <>
       <GlobalStyle />
       <Container>
-        <Head>
-          <title>Contact Card</title>
-        </Head>
-
         <Card>
+          <Avatar src={AvatarImage} width={180} height={180} />
+
           <Title>Matheus Moura</Title>
 
           <Subtitle>Dev Front-End</Subtitle>
--- styles/index.js
+++ styles/index.js
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import styled from 'styled-components'
 
 export const Container = styled.div`
@@ -16,9 +17,31 @@ export const Container = styled.div`
     #ffffff;
 `
 
-export const Card = styled.div``
+export const Card = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
 
-export const Avatar = styled.img``
+  width: 80%;
+  max-width: 483px;
+  height: 639px;
+
+  background: linear-gradient(156.58deg, #fafafa 17.17%, #c2c4cd 84.45%);
+  box-shadow: 1.55897px 1.55897px 9.35384px rgba(84, 127, 185, 0.45);
+
+  border-radius: 29.3659px;
+`
+
+export const Avatar = styled(Image)`
+  background: linear-gradient(
+    90deg,
+    rgba(9, 30, 58, 0.2) 0%,
+    rgba(47, 128, 237, 0.2) 50%,
+    rgba(45, 158, 224, 0.2) 100%
+  );
+  border-radius: 243.977px;
+`
 
 export const Title = styled.h1``
 
