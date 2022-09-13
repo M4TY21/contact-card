5b65246e68f9cca6371c561613834f1a3d30abca
Matheus <matheusegenio212@gmail.com>
2022-09-13 17:36:14 -0300

    feat and styles: finished components

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,83 @@
-import { Container, Card, Avatar, Title, Subtitle } from '../styles'
-
-import { GlobalStyle } from '../styles/global'
-
+import Head from 'next/head'
 import Image from 'next/image'
 
+import { GlobalStyle } from '../styles/global'
+import { Check, Copy } from 'react-feather'
+
+import {
+  Container,
+  Card,
+  Avatar,
+  Title,
+  Subtitle,
+  IconsDiv,
+  Icon,
+  Buttom
+} from '../styles'
+
 import AvatarImage from '../assets/avatar.jpg'
+import GithubIcon from '../assets/github.svg'
+import InstagramIcon from '../assets/instagram.svg'
+import LinkedinIcon from '../assets/linkedin.svg'
+import WhatsAppIcon from '../assets/whatsapp.svg'
+import { useState } from 'react'
 
 export default function Home() {
+  const [icon, setIcon] = useState(false)
+
+  function handleCopyEmail() {
+    navigator.clipboard.writeText('matheuseugenio212@gmail.com')
+    setIcon(true)
+    setTimeout(setIcon, 5000, false)
+  }
+
   return (
     <>
       <GlobalStyle />
+      <Head>
+        <title>Contact Card</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+
       <Container>
         <Card>
-          <Avatar src={AvatarImage} width={180} height={180} />
+          <Avatar src={AvatarImage} width={200} height={200} />
 
           <Title>Matheus Moura</Title>
 
           <Subtitle>Dev Front-End</Subtitle>
+
+          <IconsDiv>
+            <Icon href="https://github.com/M4TY21" target="_blank">
+              <Image src={GithubIcon} width={30} height={30} />
+            </Icon>
+            <Icon href="https://instagram.com/mouramths" target="_blank">
+              <Image src={InstagramIcon} width={30} height={30} />
+            </Icon>
+            <Icon
+              href="https://www.linkedin.com/in/matheus-moura-1921771b9/"
+              target="_blank"
+            >
+              <Image src={LinkedinIcon} width={30} height={30} />
+            </Icon>
+            <Icon href="http://wa.me/5584999926241" target="_blank">
+              <Image src={WhatsAppIcon} width={30} height={30} />
+            </Icon>
+          </IconsDiv>
+
+          <Buttom onClick={handleCopyEmail}>
+            Copy email
+            {icon ? (
+              <Check size={24} color="white" />
+            ) : (
+              <Copy size={24} color="white" />
+            )}
+          </Buttom>
         </Card>
       </Container>
     </>
--- styles/index.js
+++ styles/index.js
@@ -19,30 +19,88 @@ export const Container = styled.div`
 
 export const Card = styled.div`
   display: flex;
-  justify-content: center;
   align-items: center;
   flex-direction: column;
+  padding: 80px;
 
-  width: 80%;
-  max-width: 483px;
+  width: 25%;
+  min-width: 400px;
   height: 639px;
 
-  background: linear-gradient(156.58deg, #fafafa 17.17%, #c2c4cd 84.45%);
-  box-shadow: 1.55897px 1.55897px 9.35384px rgba(84, 127, 185, 0.45);
+  background: linear-gradient(156.58deg, #fafafa 17.17%, #c2c4cd 84.45%),
+    linear-gradient(90deg, #091e3a 0%, #2f80ed 50%, #2d9ee0 100%);
+  box-shadow: 1.83537px 1.83537px 22.0245px rgba(84, 127, 185, 0.2);
 
   border-radius: 29.3659px;
 `
 
 export const Avatar = styled(Image)`
-  background: linear-gradient(
-    90deg,
-    rgba(9, 30, 58, 0.2) 0%,
-    rgba(47, 128, 237, 0.2) 50%,
-    rgba(45, 158, 224, 0.2) 100%
-  );
-  border-radius: 243.977px;
+  border-radius: 50%;
 `
 
-export const Title = styled.h1``
+export const Title = styled.h1`
+  margin-top: 1rem;
 
-export const Subtitle = styled.h2``
+  font-family: 'Montserrat', sans-serif;
+  font-weight: 700;
+  font-size: 2rem;
+  white-space: nowrap;
+
+  color: #1d57a5;
+`
+
+export const Subtitle = styled.h2`
+  font-family: 'Montserrat', sans-serif;
+  font-weight: 400;
+  font-size: 1.5rem;
+
+  color: #1d69cf;
+`
+
+export const IconsDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 1rem;
+  margin-top: 1.5rem;
+`
+
+export const Icon = styled.a`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0.6rem;
+
+  background: linear-gradient(
+      90deg,
+      #091e3a -31.08%,
+      #2f80ed 94.97%,
+      #2d9ee0 148.65%
+    ),
+    #ffffff;
+  border-radius: 50%;
+
+  cursor: pointer;
+`
+
+export const Buttom = styled.button`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 40px;
+  padding: 0 35px;
+
+  width: 80%;
+  min-width: 258px;
+  height: 70px;
+
+  border: 0;
+  border-radius: 118.168px;
+  background: linear-gradient(90deg, #091e3a 0%, #2f80ed 50%, #2d9ee0 100%);
+
+  font-family: 'Montserrat', sans-serif;
+  font-weight: 600;
+  font-size: 1.5rem;
+  color: #fffefe;
+
+  cursor: pointer;
+`
